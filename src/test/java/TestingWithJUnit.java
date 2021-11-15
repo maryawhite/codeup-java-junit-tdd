53d66534f107810388e1fc28686a95c32ff3c9c9
Mary White <mary.a.white28@gmail.com>
2021-11-15 11:43:06 -0500

    following along with lecture 111521

--- src/test/java/TestingWithJUnit.java
+++ src/test/java/TestingWithJUnit.java
@@ -1,2 +1,95 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class TestingWithJUnit {
+    private List<String> names;
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello World");
+    }
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    // When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonedSheep = sheep;
+
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonedSheep);
+    }
+
+    @Test
+    public void testIfArrayEquals() {
+        char[] expected = {'J','u','n','i','t'};
+        char[] actual = "Junit".toCharArray();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);   //you'll only see the message in the event of a failure
+        assertFalse("5 is not greater than 6", 5 > 6); //remember to update your message if you update the condition
+    }
+
+    @Test
+    public void testIfInstanceIsNull() {
+        Object phone = new Object();
+        Object laptop = null;
+        assertNull(null);
+        assertNotNull(phone);
+    }
+
+    //we want to write a test that uses the ArrayList of names.
+    //we need a before method
+    @Before
+    public void setUp(){
+        this.names = new ArrayList<>();
+        this.names.add("Mary");
+    }
+
+    @Test
+    public void testIfNamesIsInitialized(){
+        assertNotNull(names);
+    }
+
+    @Test
+    public void testIfANameCanBeAdded(){
+        assertEquals(1, this.names.size());
+        this.names.add("Zach");
+        assertEquals(2, this.names.size());
+        assertSame("Mary", this.names.get(0));
+        assertSame("Zach", this.names.get(1));
+    }
+
+
 }
